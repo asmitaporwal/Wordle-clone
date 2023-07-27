e66028c20341094a8064a31cabfb48adfe524046
Asmita Porwal <asmita.porwal04@gmail.com>
2023-07-27 20:37:09 +0530

    created grid and its functionality

--- script.js
+++ script.js
@@ -15292,11 +15292,18 @@ const dictionary = [
 
 ]
 
-const WORD_LENGTH = 5;
+const WORD_LENGTH = 5
+const FLIP_ANIMATION_DURATION = 500
+const keyboard = document.querySelector("[data-keyboard]")
+const alertContainer = document.querySelector("[data-alert-container]")
 const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2023, 0, 1)
+const msOffset = Date.now() - offsetFromDate
+const dayoffset = msOffset / 1000 / 60 / 60 / 24
+console.log(Math.floor(dayoffset))
+const targetWord = targetWords[Math.floor(dayoffset)]
 
 startInteraction()
-
 function startInteraction() {
     document.addEventListener("click", handleMouseClick);
     document.addEventListener("keydown", handleKeyPress);
@@ -15304,7 +15311,7 @@ function startInteraction() {
 
 function stopInteraction() {
     document.removeEventListener("click", handleMouseClick);
-    document.removeListener("keydown", handleKeyPress);
+    document.removeEventListener("keydown", handleKeyPress);
 }
 
 function handleMouseClick(e) {
@@ -15326,7 +15333,7 @@ function handleMouseClick(e) {
 
 function handleKeyPress(e) {
     if (e.key === "Enter") {
-        submitGusess()
+        submitGuess()
         return
     }
 
@@ -15359,6 +15366,59 @@ function deletekey() {
     delete lastTile.dataset.letter
 }
 
+function submitGuess() {
+    const activeTiles = [...getActiveTiles()]
+    if (activeTiles.length !== WORD_LENGTH) {
+        showAlert("Not enough letters")
+        shakeTiles(activeTiles)
+        return
+    }
+
+    const guess = activeTiles.reduce((word, tile) => {
+        return word + tile.dataset.letter
+    }, "")
+    if (!dictionary.includes(guess)) {
+        showAlert("Not in word list")
+        shakeTiles(activeTiles)
+        return
+    }
+    stopInteraction()
+    activeTiles.forEach((...params) => flipTiles(...params, guess))
+}
+
+function flipTiles(tile, index, array, guess) {
+    const letter = tile.dataset.letter
+    const key = keyboard.querySelector(`[data-key="${letter}"]`)
+    setTimeout(() => {
+        tile.classList.add("flip")
+    }, (index * FLIP_ANIMATION_DURATION) / 2)
+}
+
 function getActiveTiles() {
     return guessGrid.querySelectorAll('[data-state="active"]')
 }
+
+function showAlert(message, duration = 1000) {
+    const alert = document.createElement("div")
+    alert.textContent = message
+    alert.classList.add("alert")
+    alertContainer.prepend(alert)
+    if (duration == null) return
+
+    setTimeout(() => {
+        alert.classList.add("hide")
+        alert.addEventListener("transitionend", () => {
+            alert.remove()
+        })
+    }, duration)
+}
+
+function shakeTiles(tiles) {
+    tiles.forEach(tile => {
+        tile.classList.add("shake")
+        tile.addEventListener("animationend", () => {
+            tile.classList.remove("shake")
+        }, { once: true })
+    })
+}
+
